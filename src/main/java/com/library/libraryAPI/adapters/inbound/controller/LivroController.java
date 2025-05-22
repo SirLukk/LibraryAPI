d963c7ebef47590108cf1aacb0ef69f715060108
John Luke <lucas.belda@outlook.com>
2025-05-22 02:03:52 -0300

    feat(Controller): Implementa LivroController com os endpoints GET, POST, PUT e DELETE

--- src/main/java/com/library/libraryAPI/adapters/inbound/controller/LivroController.java
+++ src/main/java/com/library/libraryAPI/adapters/inbound/controller/LivroController.java
@@ -1,22 +1,58 @@
 package com.library.libraryAPI.adapters.inbound.controller;
 
 
-import com.library.libraryAPI.adapters.outbound.repositories.LivroRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.library.libraryAPI.domain.livro.Livro;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation. *;
+import java.util.List;
+import java.util.Optional;
 
 @RequestMapping("/api/v1")
 @RestController
 public class LivroController {
 
-    @Autowired
-    private LivroRepository livroRepository;
+    private final LivroService service;
 
     @GetMapping
-    public String HelloWorld(){
-        return "What the heck";
+    public ResponseEntity <List<Livro>>listaLivros(){
+        List<Livro> livros = service.listaLivros();
+        return ResponseEntity.ok(livros);
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<Livro> buscaLivro(@PathVariable Long id){
+
+        Optional<Livro> livro = service.buscaLivro(id);
+        return livro
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+
+    }
+
+    @PostMapping
+    public ResponseEntity<Livro> registraLivro(@RequestBody Livro livro){
+
+        Livro livroRegistrado = service.registraLivro(livro);
+        return ResponseEntity.ok(livroRegistrado);
+
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Livro> atualizaLivro(@PathVariable Long id, @RequestBody Livro livro){
+
+        Optional<Livro> livroAtualizado = service.AtulizaLivro(id, livro);
+        return livroAtualizado
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletaLivro(@PathVariable Long id) {
+        boolean deletou = service.deletaLivro(id);
+        if (deletou) {
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
